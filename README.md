9fa17f6aeead9fc9073ceb9cee7a00234af2fb4c
bgoff <bgoff@tpa.envu.com>
2014-02-18 17:34:19 -0500

    Add option to disable replication

--- README.md
+++ README.md
@@ -9,7 +9,11 @@ ROOT_PASS=pgpass # sets the root password
 REPLICATION_USER=replication # sets the replication user
 REPLICATION_PASS=replpass # sets the replication user's password
 MASTER_HOST=$MASTER_PORT_5432_TCP_ADDR # sets the master host address
-PG_CONFIG # Passes into postgres process
+PG_CONFIG= # Passes into postgres process
+NO_REPLICATION= # Disables setting up replication user if set
 ```
+Make sure to look at and change these defaults as they are not secure.<br />
+Give special attention to the NO_REPLICATION setting, if this is NOT set then a replication user will be created
+
 You can either pass these options into the docker or add your customisations to /etc/default/postgres
 
--- pg_start.sh
+++ pg_start.sh
@@ -19,17 +19,20 @@ if [ -z $MASTER_HOST ]; then
   MASTER_HOST=$MASTER_PORT_5432_TCP_ADDR
 fi
 
-if [ ! -e /var/lib/postgresql/db_setup ]; then
-  sysctl -w kernel.shmmax=4418740224 && /etc/init.d/postgresql start && su postgres -c "createuser -s -d root && psql -c \"ALTER USER root with PASSWORD '${ROOT_PASS}'; CREATE USER ${REPLICATION_USER} REPLICATION LOGIN PASSWORD '${REPLICATION_PASS}'\"" && touch /var/lib/postgresql/db_setup
-  /etc/init.d/postgresql stop
+if [ -z $NO_REPLICATION ] ; then
+  if [ ! -e /var/lib/postgresql/db_setup ]; then
+    sysctl -w kernel.shmmax=4418740224 && /etc/init.d/postgresql start && su postgres -c "createuser -s -d root && psql -c \"ALTER USER root with PASSWORD '${ROOT_PASS}'; CREATE USER ${REPLICATION_USER} REPLICATION LOGIN PASSWORD '${REPLICATION_PASS}'\"" && touch /var/lib/postgresql/db_setup
+    /etc/init.d/postgresql stop
+  fi
+
+  if [[ ! -z "$MASTER_HOST" ]]; then
+    conn_info="host=${MASTER_HOST} user=${REPLICATION_USER} password=${REPLICATION_PASS}"
+
+    echo "primary_conninfo = '${conn_info}'" > /var/lib/postgresql/9.1/main/recovery.conf
+    echo "standby_mode = 'on'" >> /var/lib/postgresql/9.1/main/recovery.conf
+  fi
 fi
 
-if [[ ! -z "$MASTER_HOST" ]]; then
-  conn_info="host=${MASTER_HOST} user=${REPLICATION_USER} password=${REPLICATION_PASS}"
-
-  echo "primary_conninfo = '${conn_info}'" > /var/lib/postgresql/9.1/main/recovery.conf
-  echo "standby_mode = 'on'" >> /var/lib/postgresql/9.1/main/recovery.conf
-fi
 sysctl -w kernel.shmmax=4418740224
 chown -R postgres.postgres /var/lib/postgresql
 
