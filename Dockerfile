9d7a98e50c273f29379bb2a3d842ff6e96122d5a
bgoff <bgoff@tpa.envu.com>
2014-02-18 17:21:45 -0500

    Make more configurable

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,10 @@ ADD pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
 RUN chown postgres.postgres /etc/postgresql/9.1/main/pg_hba.conf
 ADD postgresql.conf /etc/postgresql/9.1/main/postgresql.conf
 RUN chown postgres.postgres /etc/postgresql/9.1/main/postgresql.conf
-RUN echo "kernel.shmmax = 4418740224" >> /etc/sysctl.conf && sysctl -p
-RUN sysctl -w kernel.shmmax=4418740224 && /etc/init.d/postgresql start && su postgres -c "createuser -s -d root && psql -c \"ALTER USER root with PASSWORD 'pgpass'; CREATE USER replication REPLICATION LOGIN CONNECTION LIMIT 1 ENCRYPTED PASSWORD 'replpass'\""
 
 EXPOSE 5432
 VOLUME /var/lib/postgresql
 ADD pg_start.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/pg_start.sh
 
-CMD ["/usr/local/bin/pg_start.sh"]
+ENTRYPOINT ["/usr/local/bin/pg_start.sh"]
--- pg_start.sh
+++ pg_start.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
+if [ -e /etc/default/postgres ]; then
+  . /etc/default/postgres
+fi
 
-if [[ ! -z "$MASTER_PORT_5432_TCP_ADDR" ]]; then
-	conn_info="host=${MASTER_PORT_5432_TCP_ADDR} user=replication password=${REPLICATION_PASS}"
+if [ -z $ROOT_PASS ]; then
+  export ROOT_PASS=pgpass
+fi
 
-	echo "primary_conninfo = '${conn_info}'" > /var/lib/postgresql/9.1/main/recovery.conf
-	echo "standby_mode = 'on'" >> /var/lib/postgresql/9.1/main/recovery.conf 
+if [ -z $REPLICATION_USER ]; then
+  export REPLICATION_USER=replication
+fi
+
+if [ -z $REPLICATION_PASS ]; then
+  export REPLICATION_PASS=replpass
+fi
+
+if [ -z $MASTER_HOST ]; then
+  MASTER_HOST=$MASTER_PORT_5432_TCP_ADDR
+fi
+
+if [ ! -e /var/lib/postgresql/db_setup ]; then
+  sysctl -w kernel.shmmax=4418740224 && /etc/init.d/postgresql start && su postgres -c "createuser -s -d root && psql -c \"ALTER USER root with PASSWORD '${ROOT_PASS}'; CREATE USER ${REPLICATION_USER} REPLICATION LOGIN PASSWORD '${REPLICATION_PASS}'\"" && touch /var/lib/postgresql/db_setup
+  /etc/init.d/postgresql stop
+fi
+
+if [[ ! -z "$MASTER_HOST" ]]; then
+  conn_info="host=${MASTER_HOST} user=${REPLICATION_USER} password=${REPLICATION_PASS}"
+
+  echo "primary_conninfo = '${conn_info}'" > /var/lib/postgresql/9.1/main/recovery.conf
+  echo "standby_mode = 'on'" >> /var/lib/postgresql/9.1/main/recovery.conf
 fi
 sysctl -w kernel.shmmax=4418740224
-su postgres -c "/usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf $PG_CONFIG"
+chown -R postgres.postgres /var/lib/postgresql
+
+exec su postgres -c "/usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf $PG_CONFIG"
